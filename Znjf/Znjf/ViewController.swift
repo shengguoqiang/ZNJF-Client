b79ec6da32ee6ff27c12e91a55500eca6cec4fc2
shengguoqiang <2420548376@qq.com>
2018-09-29 17:30:47 +0800

    中南金服App客户端项目启动

---
+++ Znjf/Znjf/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Znjf
+//
+//  Created by FTD on 2018/9/29.
+//  Copyright © 2018年 江苏投吧金融服务有限公司. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("HELLO WORLD!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
