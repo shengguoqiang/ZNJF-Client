09e63074f8f1166b9266c892ba567c342fe26a44
dqs <sgq_email@163.com>
2018-10-22 17:40:06 +0800

    添加原生与js交互

--- Znjf/Znjf/ZNJFWebViewController.swift
+++ Znjf/Znjf/ZNJFWebViewController.swift
@@ -18,12 +18,12 @@ class ZNJFWebViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print("width = \(UIScreen.main.bounds.width)")
-        print("height = \(UIScreen.main.bounds.height)")
         webView = WKWebView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height), configuration: WKWebViewConfiguration())
         webView.navigationDelegate = self
         webView.uiDelegate = self
         view.addSubview(webView)
+        
+        setup()
         
     }
     
@@ -32,6 +32,29 @@ class ZNJFWebViewController: UIViewController {
         webView.load(request!)
     }
 
+}
+
+extension ZNJFWebViewController {
+    
+    //MARK: - 初始化设置
+    func setup() {
+        //注册js与原生交互d对象
+        let userContentCtl = webView.configuration.userContentController
+        userContentCtl.add(self, name: "ZNJFEventHandler")
+    }
+    
+    //MARK: - js与原生交互方法
+    //判断客户端是否登录
+    func judgeLogin(callBack: (String)->Void) {
+        let token = "dqs"
+        callBack(token)
+    }
+    
+    //toast提示
+    func gotoShowToast(param: Dictionary<String, String>?) {
+        print(param!["msg"] ?? "")
+    }
+    
 }
 
 extension ZNJFWebViewController: WKNavigationDelegate {
@@ -57,5 +80,24 @@ extension ZNJFWebViewController: WKUIDelegate {
     func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
         print("message = \(message)")
         completionHandler()
+    }
+}
+
+extension ZNJFWebViewController: WKScriptMessageHandler {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if message.name == "ZNJFEventHandler" {
+            if let msgDic = message.body as? Dictionary<String, Any> {
+                let methodName = msgDic["methodName"] as! String
+                let params = msgDic["params"] as? Dictionary<String, String>
+                let callBackID = msgDic["callBackID"] as? String
+                if methodName == "judgeLogin" {//判断客户端是否登录
+                    judgeLogin { (token) in
+                        webView.evaluateJavaScript("ZNJFEventHandler.callBack('\(callBackID!)', '\(token)')", completionHandler: nil)
+                    }
+                } else if methodName == "gotoShowToast" {//toast提示
+                    gotoShowToast(param: params)
+                }
+            }
+        }
     }
 }
